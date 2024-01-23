d4da5d4e582ca45df0e5b6f0e598b3a4a0414c5f
Maudit00 <davide9311@gmail.com>
2024-01-23 16:59:39 +0100

    init

--- src/main/java/it/epicode/entities/Evento.java
+++ src/main/java/it/epicode/entities/Evento.java
@@ -2,10 +2,19 @@ package it.epicode.entities;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table (name = "eventi")
 public class Evento {
     @Id
     @GeneratedValue ( strategy = GenerationType.AUTO)
-    @Column
+    @Column (name = "id")
+    private int id;
+
+    @Column (name = "titolo")
+    private String titolo;
+    @Column (name = "data_evento")
+    private LocalDate data_evento;
+
 }
